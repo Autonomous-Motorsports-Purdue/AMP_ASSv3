0ed2397305835426744beb08e495335f7e4e3ca1
alanssitis <alanchunggt@gmail.com>
2022-12-22 21:15:07 -0500

    amp_launch_tests: initialize package
    amp_launch_tests contains launch files required for testing.

--- src/amp_launch_tests/launch/teleop_joy_micro_ros.py
+++ src/amp_launch_tests/launch/teleop_joy_micro_ros.py
@@ -1,5 +1,9 @@
+from ament_index_python.packages import get_package_share_path
+
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description import DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -9,6 +13,11 @@ def generate_launch_description():
         DeclareLaunchArgument(name='serial_tty',
                               default_value='/dev/ttyACM0',
                               description='Serial TTY absolute file location'),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                str(
+                    get_package_share_path('teleop_twist_joy') / 'launch' /
+                    'teleop-launch.py'))),
         Node(package='micro_ros_agent',
              executable='micro_ros_agent',
              name='micro_ros_agent',
