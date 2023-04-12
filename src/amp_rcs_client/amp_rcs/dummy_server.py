5622853bb6ed0bb735d9d411f48e5620e06aaa01
berliernj <berlier3@gmail.com>
2023-04-12 15:51:28 -0400

    Initial Commit

---
+++ src/amp_rcs_client/amp_rcs/dummy_server.py
@@ -0,0 +1,28 @@
+'''ROS2 service server for behavioral determination based on client call'''
+
+#usr/bin/env python3
+from rcs_service.srv import TrackState
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class ServiceServer(Node):
+	def __init__(self):
+		super().__init__('service_server')
+		self.server = self.create_service(TrackState, 'track_state', self.track_state_callback)
+	
+	def track_state_callback(self, request, response):
+		response.state = request.state
+		self.get_logger().info('Recieved: %s' % (request.state))
+		self.get_logger().info('Response: %s\n' % (response.state))
+		
+		return response
+
+def main(args=None):
+	rclpy.init(args=args)
+	node = ServiceServer()
+	rclpy.spin(node)
+	rclpy.shutdown()
+	
+if __name__ == 'main':
+	main()
