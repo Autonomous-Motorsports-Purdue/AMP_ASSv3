d36ebcceb1034b79f83093620e7c8720243cb6ae
reschivon <xchenbox@gmail.com>
2023-04-30 02:26:51 -0400

    amp_bt_seg: Add static tf broadcast to launch
    Also upgraded PCL to 1.7

--- src/amp_kart_segmentation/launch/segmentation.launch.py
+++ src/amp_kart_segmentation/launch/segmentation.launch.py
@@ -10,11 +10,23 @@ def generate_launch_description():
     share_dir = get_package_share_directory('amp_kart_segmentation')
     params_dir = os.path.join(share_dir, 'params', 'segmentation.params.yaml')
 
-    segmentation_node = Node(package='amp_kart_segmentation',
-                             executable='segmentation',
-                             parameters=[params_dir],
-                             name='RANSACsegmentation')
+    tf_pub = Node(name="base_tf_pub",
+                  package="tf2_ros",
+                  executable="static_transform_publisher",
+                  arguments=["0", "0", "0", "0", "0", "0", "map", "velodyne"])
 
-    ld = LaunchDescription([segmentation_node])
+    segmentation_node = Node(
+        package='amp_kart_segmentation',
+        executable='segmentation',
+        parameters=[params_dir],
+        remappings=[('~/input', '/velodyne_points'),
+                    ('~/output', '/nonground')],
+        name='RANSACsegmentation',
+        output='screen',
+        emulate_tty=True,
+        #  prefix=["xterm -e gdb ex run --args"]
+    )
+
+    ld = LaunchDescription([segmentation_node, tf_pub])
 
     return ld
