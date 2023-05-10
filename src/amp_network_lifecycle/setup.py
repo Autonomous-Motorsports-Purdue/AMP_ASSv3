e28926facaf83c8b3b0d437d142f07c3a05faec8
berliernj <berlier3@gmail.com>
2023-05-10 13:17:49 -0400

    resolved pr issues

--- src/amp_network_lifecycle/setup.py
+++ src/amp_network_lifecycle/setup.py
@@ -4,7 +4,7 @@ package_name = 'amp_network_lifecycle'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,9 +15,8 @@ setup(
     zip_safe=True,
     maintainer='berliernj',
     maintainer_email='berlier3@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='Checks connection to the network. E-stops if disconnected for 5 seconds',
+    license='GNU General Public License 3.0',
     entry_points={
         'console_scripts': [
             "lifecycle_client = amp_network_lifecycle.network_lifecycle_client:main",
